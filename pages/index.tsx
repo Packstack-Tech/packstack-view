c4fb66489559eb294c3b0037ba4455d0e851a044
maplethorpej <jeradmaplethorpe@gmail.com>
2021-07-21 21:30:27 -0500

    homepage design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,50 @@
 import Head from "next/head";
+import Image from "next/image";
+import Logo from "../public/packstack_logo_white.png";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Packstack | Backpacking list creator</title>
         <meta
           name="description"
-          content="Create packing lists for your backpacking trips."
+          content="Create and share packing lists for backpacking, camping or anything else."
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
-      <main>
-        <p>HOMEPAGE</p>
-      </main>
+      <div className={styles.Splash}>
+        <header className={styles.Header}>
+          <div className={styles.container}>
+            <div className={styles.HeaderInner}>
+              <div className={styles.Logo}>
+                <Image src={Logo} alt="Packstack logo" />
+              </div>
+              <div className={styles.BtnGroup}>
+                <a href="#" className={styles.Button}>
+                  Login
+                </a>
+                <a href="#" className={`${styles.Button} ${styles.BtnOutline}`}>
+                  Sign Up
+                </a>
+              </div>
+            </div>
+          </div>
+        </header>
+
+        <main className={styles.Hero}>
+          <div className={styles.container}>
+            <h1>Packing Lists for Backpackers, Campers and Explorers</h1>
+            <p>
+              Packstack makes it easy to plan your next adventure. Simply add
+              your gear and start building your packing list. We’ll give you a
+              weight breakdown so you can fine-tune your loadout.
+            </p>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
