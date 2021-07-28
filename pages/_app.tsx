eee4349b4b3a74dff5f141d05a4c6058763745a6
maplethorpej <jeradmaplethorpe@gmail.com>
2021-07-27 20:50:07 -0500

    google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,30 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      // @ts-ignore
+      window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+        page_path: url,
+      })
+    }
+
+    // When the component is mounted, subscribe to router changes
+    // and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
 export default MyApp
