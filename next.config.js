1cacbb694129bea07f7af66a8eb730cb2f127675
maplethorpej <jeradmaplethorpe@gmail.com>
2021-07-30 22:17:35 -0500

    wildcard route redirects

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/:slug(\\d{1,})/:path*',
-        destination: '/pack/:slug/:path*', // Matched parameters can be used in the destination
-        permanent: true,
-      },
-    ]
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/:slug(\\d{1,})/:path*',
+  //       destination: '/pack/:slug/:path*', // Matched parameters can be used in the destination
+  //       permanent: true,
+  //     },
+  //   ]
+  // },
 }
