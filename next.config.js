72f4c5080e8f7603e3d5e462630682126496b88d
maplethorpej <jeradmaplethorpe@gmail.com>
2021-08-04 11:56:06 -0700

    adds legacy redirect routes

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ module.exports = {
   async redirects() {
     return [
       {
+        source: '/register',
+        destination: 'https://app.packstack.io/register',
+        permanent: true,
+        basePath: false
+      },
+      {
+        source: '/login',
+        destination: 'https://app.packstack.io',
+        permanent: true,
+        basePath: false
+      },
+      {
         source: '/:slug(\\d{1,})/:path*',
         destination: '/pack/:slug*/:path*', // Matched parameters can be used in the destination
         permanent: true,
